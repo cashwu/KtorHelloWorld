648f59cd92ad1b376cafbecce3b365895d07ef41
cash <cashwugeek@icloud.com>
2025-05-17 16:16:16 +0800

    another endpoint with HTML

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,5 +1,6 @@
 package com.cashwu
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,6 +9,12 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/test") {
+            val text = "<h1>Hello World!</h1>"
+            val type = ContentType.Text.Html
+            call.respondText(text, type)
         }
     }
 }
